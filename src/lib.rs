cf4211a76faaee8e219e7316e2f3de383c436942
Dan <igaryhe@gmail.com>
2020-11-18 00:56:32 +0800

    a working hello world

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,48 @@ static mut PLAYDATE: Playdate = Playdate {
 
 static mut FONT: *mut sys::LCDFont = ptr::null_mut();
 
+const INIT_X: i32 = (400 - TEXT_WIDTH) / 2;
+const INIT_Y: i32 = (240 - TEXT_HEIGHT) / 2;
+
+const TEXT_WIDTH: i32 = 86;
+const TEXT_HEIGHT: i32 = 16;
+
+struct State {
+    x: i32,
+    y: i32,
+    dx: i32,
+    dy: i32,
+}
+
+impl State {
+    pub fn init(&mut self) {
+        self.x = INIT_X;
+        self.y = INIT_Y;
+        self.dx = 1;
+        self.dy = 2;
+    }
+
+    pub fn update(&mut self) {
+        unsafe {
+            PLAYDATE.graphics.unwrap().clear(graphics::LCDColor::SolidColor(sys::LCDSolidColor::kColorWhite));
+            PLAYDATE.graphics.unwrap().draw_text(FONT, ptr::null_mut(), ptr::null_mut(), "Hello World",
+                                                 sys::PDStringEncoding::kASCIIEncoding, self.x, self.y,
+                                                 sys::LCDBitmapDrawMode::kDrawModeCopy, 0,
+                                                 sys::LCDRect { left: 0, right: 0, top: 0, bottom: 0}).unwrap();
+        }
+        self.x += self.dx;
+        self.y += self.dy;
+        if self.x < 0 || self.x > sys::LCD_COLUMNS as i32 - TEXT_WIDTH {
+            self.dx = -self.dx;
+        }
+        if self.y < 0 || self.y > sys::LCD_ROWS as i32 - TEXT_HEIGHT {
+            self.dy = -self.dy;
+        }
+    }
+}
+
+static mut STATE: State = State { x: INIT_X, y: INIT_Y, dx: 1, dy: 2 };
+
 impl Playdate {
     pub fn new(playdate: *mut sys::PlaydateAPI) {
         unsafe {
@@ -41,8 +83,7 @@ impl Playdate {
 
 extern "C" fn update(_ud: *mut cty::c_void) -> cty::c_int {
     unsafe {
-        PLAYDATE.graphics.unwrap().clear(graphics::LCDColor::SolidColor(sys::LCDSolidColor::kColorWhite));
-        PLAYDATE.graphics.unwrap().draw_text(FONT, ptr::null_mut(), ptr::null_mut(), "Hello World", sys::PDStringEncoding::kASCIIEncoding, 150, 100, sys::LCDBitmapDrawMode::kDrawModeCopy, 0, sys::LCDRect { left: 0, right: 0, top: 0, bottom: 0}).unwrap();
+        STATE.update();
     }
     1
 }
