bb87849c91200d6b466cddc42873f19813daff33
Dan <igaryhe@gmail.com>
2021-06-18 00:43:09 +0800

    only require ARM_GCC when building for device

--- playdate-sys/build.rs
+++ playdate-sys/build.rs
@@ -7,7 +7,6 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let pd_sdk_path = env::var("PLAYDATE_SDK").expect("envvar PLAYDATE_SDK hasn't been set");
-    let arm_gcc = env::var("ARM_GCC").expect("envvar ARM_GCC hasn't been set");
     let builder = bindgen::Builder::default()
         .use_core()
         .ctypes_prefix("cty")
@@ -25,6 +24,7 @@ fn main() {
         .bitfield_enum("PDButtons")
         .bitfield_enum("PDPeripherals");
     let bindings = if arch == "arm" {
+        let arm_gcc = env::var("ARM_GCC").expect("envvar ARM_GCC hasn't been set");
         builder
             .clang_arg("-DTARGET_PLAYDATE=1")
             .clang_arg(format!("-I{}/include", arm_gcc))
