d3c459721fe0ae65d137e62597b84c4a62db65c7
Dan <igaryhe@gmail.com>
2021-06-17 20:44:46 +0800

    deleted an unused dependency

--- make.sh
+++ make.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ "$1" == "simulator" ];then
+if [[ "$1" == "simulator" ]];then
     out=target/release/examples
     mkdir -p $out/source
     touch $out/source/pdex.bin
     cp $out/lib$2.dylib $out/source/pdex.dylib
     mkdir -p build
     pdc $out/source build/$2.pdx
-elif [ "$1" == "device" ];then
+elif [[ "$1" == "device" ]];then
     out=target/thumbv7em-none-eabihf/release/examples
     mkdir -p $out/source
-    arm-none-eabi-objcopy -O binary $out/$2 %out/source/pdex.bin
+    arm-none-eabi-objcopy -O binary $out/$2 $out/source/pdex.bin
     mkdir -p build
     pdc $out/source build/$2-device.pdx
 fi
