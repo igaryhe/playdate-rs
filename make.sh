4a641810f59e3edefcf678684487f2b79f9950b8
Dan <igaryhe@gmail.com>
2021-06-17 20:34:33 +0800

    fixed bugs in make.sh

--- make.sh
+++ make.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-if [$1 == 'simulator']
-then
+if [ "$1" == "simulator" ];then
     out=target/release/examples
     mkdir -p $out/source
     touch $out/source/pdex.bin
     cp $out/lib$2.dylib $out/source/pdex.dylib
     mkdir -p build
     pdc $out/source build/$2.pdx
-elif [$1 == 'device']
+elif [ "$1" == "device" ];then
     out=target/thumbv7em-none-eabihf/release/examples
     mkdir -p $out/source
     arm-none-eabi-objcopy -O binary $out/$2 %out/source/pdex.bin
