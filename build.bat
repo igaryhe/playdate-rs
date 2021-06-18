d7a21a690d059c6b4537ea13fa91f239a69475fe
dan <igaryhe@gmail.com>
2021-06-18 12:09:56 +0800

    included cargo build command in build scripts

--- build.bat
+++ build.bat
@@ -2,12 +2,14 @@
 set out=%~dp0target\x86_64-pc-windows-msvc\release\examples
 set out_device=%~dp0target\thumbv7em-none-eabihf\release\examples
 if %1==simulator (
+    start /W /B cargo build --release --example %2 --target x86_64-pc-windows-msvc
     mkdir %out%\source 2> nul
     copy /y nul %out%\source\pdex.bin > nul
     copy %out%\%2.dll %out%\source\pdex.dll > nul
     mkdir %~dp0build 2> nul
     start pdc %out%\source %~dp0build\%2.pdx
 ) else if %1==device (
+    start /W /B cargo build --release --example %2 --target thumbv7em-none-eabihf
     mkdir %out_device%\source 2> nul
     start arm-none-eabi-objcopy -O binary %out_device%\%2 %out_device%\source\pdex.bin
     mkdir %~dp0build 2> nul
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ "$1" == "simulator" ]];then
+    cargo build --release --example $2 --target x86_64-apple-darwin
     out=target/x86_64-apple-darwin/release/examples
     mkdir -p $out/source
     touch $out/source/pdex.bin
@@ -7,6 +8,7 @@ if [[ "$1" == "simulator" ]];then
     mkdir -p build
     pdc $out/source build/$2.pdx
 elif [[ "$1" == "device" ]];then
+    cargo build --release --example $2 --target thumbv7em-none-eabihf
     out=target/thumbv7em-none-eabihf/release/examples
     mkdir -p $out/source
     arm-none-eabi-objcopy -O binary $out/$2 $out/source/pdex.bin
